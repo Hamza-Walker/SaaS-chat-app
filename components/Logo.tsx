c8b82d5a3a6ad7fea2ef317daec98d8df817688c
hamza walker <hamzawalker.vienna@gmail.com>
2024-04-17 15:51:54 +0200

    app initialisation

---
+++ components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logoImage from "@logos/black.png" 
+function Logo() {
+  return (
+    <div>
+			<img src={logoImage} alt="logo" />
+		</div>
+  )
+}
+
+export default Logo
