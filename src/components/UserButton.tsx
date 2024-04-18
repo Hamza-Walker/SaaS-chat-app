130a15ed3a21cef4a3f6005c52b34a51412552b0
hamza walker <hamzawalker.vienna@gmail.com>
2024-04-18 22:55:28 +0200

    sign out button

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -11,7 +11,7 @@ import {
 import UserAvatar from "./UserAvatar";
 import { Session } from "next-auth";
 import { Button } from "./ui/button";
-import { signIn } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 
 function UserButton({ session }: { session: Session | null }) {
 	if (!session) {
@@ -32,12 +32,9 @@ function UserButton({ session }: { session: Session | null }) {
 						/>
 					</DropdownMenuTrigger>
 					<DropdownMenuContent>
-						<DropdownMenuLabel>My Account</DropdownMenuLabel>
+						<DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
 						<DropdownMenuSeparator />
-						<DropdownMenuItem>Profile</DropdownMenuItem>
-						<DropdownMenuItem>Billing</DropdownMenuItem>
-						<DropdownMenuItem>Team</DropdownMenuItem>
-						<DropdownMenuItem>Subscription</DropdownMenuItem>
+						<DropdownMenuItem onClick={()=> signOut()}>Sign Out </DropdownMenuItem>
 					</DropdownMenuContent>
 				</DropdownMenu>
 			)
