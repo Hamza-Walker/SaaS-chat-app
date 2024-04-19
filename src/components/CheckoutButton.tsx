6faf0b9420482bf5df29bba5e0ded27128cd7c40
hamza walker <hamzawalker.vienna@gmail.com>
2024-04-20 01:35:50 +0200

    added styling to the checkoutbutotn

--- src/components/CheckoutButton.tsx
+++ src/components/CheckoutButton.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function CheckoutButton() {
-  return (
-    <div>ChecoutButton</div>
-  )
+	return (
+		<div className="flex flex-col space-y-2">
+			{/* if subscribed show me the user is subscribed */}
+			<button className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default">
+				Sign Up
+			</button>
+		</div>
+	);
 }
 
-export default CheckoutButton
+export default CheckoutButton;
