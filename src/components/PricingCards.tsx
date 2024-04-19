ed89be080afadbae2e35267ffe5898e25896257f
hamza walker <hamzawalker.vienna@gmail.com>
2024-04-20 00:47:01 +0200

    Registration Page    • created component, get the session to render the users name.    - styled the page.

--- src/components/PricingCards.tsx
+++ src/components/PricingCards.tsx
@@ -87,7 +87,7 @@ const PricingCards = ({ redirect } :{ redirect : boolean}) => {
              ))}
            </ul>
            {redirect ? (
-             <Link href="/register" className="text-indigo-600">
+             <Link href="/register" className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 foucus-visible-offset-2 focus-visible:outline-indigo-500 cursor-pointer disabled:opacity-80">
                  Get started today
              </Link>
            ) : (
