aa56a1db0c1bb325484b42a756eceb699d82dc11
hamza walker <hamzawalker.vienna@gmail.com>
2024-04-20 12:34:33 +0200

    fixd a typo

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -18,7 +18,7 @@ async function RegistrationPage() {
 						viewBox="0 0 1024 1024"
 						className="absolute -top-10 left-1/2 -z-10 h-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:-top-12 md:-top-20 lg:-top-12 xl:top-0"
 					>
-						<ecllipse
+						<ellipse
 							cx={604}
 							cy={512}
 							fill="url(#radial-gradient)"
